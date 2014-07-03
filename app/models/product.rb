5d02d43697f012770043f2b80928f0b2b37b0733
macheteshuffle <jeff.li029@gmail.com>
2014-07-02 21:48:51 -0400

    caching products for re-rendering only updated products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,9 @@ class Product < ActiveRecord::Base
   validates_presence_of :title, message: "Please enter a title"
   validates_presence_of :description, message: "Please enter a description"
   validates_presence_of :price, message: "Please enter the price"
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
+
 end
