6eddace18384e5cea6092caa6df8ec244d40b522
macheteshuffle <jeff.li029@gmail.com>
2014-07-05 11:20:02 -0400

    removed unnecessary migration

--- db/migrate/20140701031054_refine_product_model.rb
+++
@@ -1,9 +0,0 @@
-class RefineProductModel < ActiveRecord::Migration
-  def up
-    rename_column :products, :descript, :description
-  end
-
-  def down
-    rename_column :products, :description, :descript
-  end
-end
