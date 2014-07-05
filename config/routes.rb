4a0e1063cb183fa2b3c92a12bc853d38499bf91b
macheteshuffle <jeff.li029@gmail.com>
2014-07-05 10:19:56 -0400

    created lineitem and cart models and added method to ensure product has no line items before its destroyed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Depot::Application.routes.draw do
+  resources :line_items
+
+  resources :carts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
