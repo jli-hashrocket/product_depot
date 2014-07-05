d4a2d17a8d6ec9450e872d2dff198f51ea181066
macheteshuffle <jeff.li029@gmail.com>
2014-07-05 10:18:21 -0400

    added session counter

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,10 @@
 class StoreController < ApplicationController
+  include SessionCounter
+
+  before_action :count_sessions, only: [:index]
+
   def index
     @products = Product.order(:title)
+    @sessions_count = session[:counter]
   end
 end
